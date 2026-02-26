ce5b0ea41312038d39136383c81474fdabe09f6c
NOAHSDELLLAPTOP\noaha <noahafriat11@gmail.com>
2026-02-26 11:36:10 -0500

    initial project skeleton with Spring Boot and Vue.js

---
+++ backend/src/test/java/ca/concordia/summs/SummsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.concordia.summs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
